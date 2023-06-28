058f3895b64c27a5126c19cdabc926d53bdef32d
Gonelo <gonzacornejo360@gmail.com>
2023-06-28 16:23:28 -0300

    Agregar archivos de proyecto.

---
+++ learnaid-backend/Data/DataContext.cs
@@ -0,0 +1,39 @@
+﻿namespace learnaid_backend.Data
+{
+    using learnaid_backend.Core.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Survey> Survey { get; set; }
+
+        public DbSet<User> User { get; set; }
+
+        /// <summary>
+        ///     Se utiliza para la construccion de los modelos con Entity Framework.
+        ///     Esta estrategia es mas conocida como fluent api, y la utilizo solamente cuando
+        ///     no existen anotaciones para funciones especificas que se pueden logran con dicha 
+        ///     estrategia.
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Survey>()
+                .Property(b => b.Id)
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<Survey>()
+               .Property(b => b.OptionsList)
+               .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<List<string>>(v)
+                );
+        }
+    }
+}
+}
